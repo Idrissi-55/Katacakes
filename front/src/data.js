1a27ca9b25a0f1b8c690626824b076a6745defe6
Idrissi-55 <idrissialamiyassine@gmail.com>
2021-10-26 12:16:05 +0200

    added more data

--- front/src/data.js
+++ front/src/data.js
@@ -1,9 +1,88 @@
 
 
-export default [{
-  id: 1,
-  name: 'Cake 1',
-  thumbnail: '/images/Cake 1.jpg',
-  season: 'Summer',
-  description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
-}]
+export default [
+  {
+    id: 1,
+    name: 'Cake 1',
+    thumbnail: '/images/Cake 1.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 2,
+    name: 'Cake 2',
+    thumbnail: '/images/Cake 2.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 3,
+    name: 'Cake 3',
+    thumbnail: '/images/Cake 3.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 4,
+    name: 'Cake 4',
+    thumbnail: '/images/Cake 4.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 5,
+    name: 'Cake 5',
+    thumbnail: '/images/Cake 5.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 6,
+    name: 'Cake 6',
+    thumbnail: '/images/Cake 6.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 7,
+    name: 'Cake 7',
+    thumbnail: '/images/Cake 7.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 8,
+    name: 'Cake 8',
+    thumbnail: '/images/Cake 8.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 9,
+    name: 'Cake 9',
+    thumbnail: '/images/Cake 9.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id:10,
+    name: 'Cake10',
+    thumbnail: '/images/Cake 10.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 11,
+    name: 'Cake 11',
+    thumbnail: '/images/Cake 11.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  },
+  {
+    id: 12,
+    name: 'Cake 12',
+    thumbnail: '/images/Cake 12.jpg',
+    season: 'Summer',
+    description: 'Lorem ipsum dolor sit amet. Hic galisum quod et repudiandae aperiam eum alias autem ut omnis minus sit dolores assumenda ut doloremque voluptatem. Sit galisum placeat in assumenda alias qui voluptas voluptatem sed reiciendis voluptatem qui similique ipsa rem quia pariatur et neque necessitatibus.'
+  }
+]
